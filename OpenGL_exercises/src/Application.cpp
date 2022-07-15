712adde8a2d01fb9f1e147ae027a5439e3e9eb33
Previ0 <tuckipatryk@gmail.com>
2022-07-15 06:45:44 +0200

    Error handling

--- OpenGL_exercises/src/Application.cpp
+++ OpenGL_exercises/src/Application.cpp
@@ -10,6 +10,26 @@
 // https://www.youtube.com/watch?v=0p9VxImr7Y0&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=4
 // https://shot511.github.io/2014-06-08-tutorial-04-czym-jest-programowalny-potok-renderowania/
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+
+static void GLClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line) {
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL error] (" << error << "): " << function << " " <<
+            file << ": " << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
 struct ShaderProgramSource {
     std::string VertexSource;
     std::string FragmentSource;
@@ -139,7 +159,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         // glDrawArrays(GL_TRIANGLES, 0, 6); - if there is only data buffer to draw
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
